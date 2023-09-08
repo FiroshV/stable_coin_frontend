666ed9ee7efc8e5606d790e0206bf87ea5877a90
FiroshV <firosh.vasudevan@gmail.com>
2023-09-08 10:45:27 +0530

    first commit

--- README.md
+++ README.md
@@ -1,38 +1,63 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Staking App
 
-## Getting Started
+This is a simple staking application that allows users to stake their tokens and track their staked duration. The application is built using React and the ethers.js library to interact with the Ethereum blockchain. The application is set up to work with the Fantom testnet.
 
-First, run the development server:
+## Features
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+- Display the APR (Annual Percentage Rate) for staking.
+- Allow users to stake and unstake tokens.
+- Display the elapsed time since the user staked their tokens.
+- Dynamically calculate and refresh the APR.
+  
+## Prerequisites
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- Node.js and npm installed.
+- MetaMask or a similar Ethereum wallet browser extension for interacting with the app.
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+## Quick Start
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+1. **Clone the repository**:
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+   ```bash
+   git clone [repository-url]
+   ```
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+2. **Navigate to the project directory**:
 
-## Learn More
+   ```bash
+   cd fantom-stake-portal
+   ```
 
-To learn more about Next.js, take a look at the following resources:
+3. **Install the required dependencies**:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+   ```bash
+   npm install
+   ```
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+4. **Run the development server**:
 
-## Deploy on Vercel
+   ```bash
+   npm run dev
+   ```
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+5. **Open a web browser** and navigate to `http://localhost:3000` to access the Staking App.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Minting Staking Tokens
+
+Before staking tokens, you may need to mint or acquire the staking tokens used in this application. You can interact with the token contract and mint tokens using the following link:
+
+[https://testnet.ftmscan.com/address/0x5b96e18cff249cf70f35f034e7606e850d5cda6c](https://testnet.ftmscan.com/address/0x5b96e18cff249cf70f35f034e7606e850d5cda6c)
+
+## Contract Details
+
+The staking contract used in this application is available for review and interaction on the Fantom testnet. You can access the contract and its code using the following link:
+
+[https://testnet.ftmscan.com/address/0x8699d52c6c82b588bc12b46ada750dc0eb298818](https://testnet.ftmscan.com/address/0x8699d52c6c82b588bc12b46ada750dc0eb298818)
+
+## Code Structure
+
+The main code resides in the `Home` component. The component sets up the connection with the Ethereum blockchain, allows users to stake and unstake their tokens, and calculates/display the APR.
+
+## Errors and Logging
+
+Make sure to monitor the browser console for any errors or logs during the staking/unstaking processes. The application provides appropriate error messages and logs for better debugging.
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,230 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState, useEffect, useRef } from "react";
+const { ethers } = require("ethers");
+import contractABI from "../ABI.json";
+import ERC20_ABI from "../ERC20_ABI.json";
 
-const inter = Inter({ subsets: ['latin'] })
+const contractAddress = "0x8699d52C6c82B588Bc12B46aDA750dC0EB298818";
+const TOKEN_ADDRESS = "0x5b96E18Cff249Cf70F35F034E7606E850D5CDa6C";
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [amount, setAmount] = useState(0);
+  const [provider, setProvider] = useState(null);
+
+  const [protocolAPR, setAPR] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState("0s");
+
+  const timerIntervalRef = useRef(null);
+
+  const elapsedSecondsRef = useRef(0);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (window && window.ethereum) {
+      setProvider(new ethers.BrowserProvider(window.ethereum));
+    }
+  }, []);
+
+  useEffect(() => {
+    calculateAPR();
+  }, [provider]);
+
+  const formatElapsedTime = (totalSeconds) => {
+    const seconds = totalSeconds % 60;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const days = Math.floor(totalSeconds / 86400);
+
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  };
+
+  const startTimer = () => {
+    elapsedSecondsRef.current = 0;
+    setElapsedTime(formatElapsedTime(0));
+
+    timerIntervalRef.current = setInterval(() => {
+      elapsedSecondsRef.current += 1;
+      setElapsedTime(formatElapsedTime(elapsedSecondsRef.current));
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    if (timerIntervalRef.current) {
+      clearInterval(timerIntervalRef.current);
+      timerIntervalRef.current = null;
+      elapsedSecondsRef.current = 0; 
+      setElapsedTime(formatElapsedTime(0));
+    }
+  };
+
+  const initializeContract = (address, ABI, signer) => {
+    return new ethers.Contract(address, ABI, signer || provider);
+  };
+
+  async function handleApprove(signer, amountToStake) {
+    try {
+      const tokenContract = initializeContract(
+        TOKEN_ADDRESS,
+        ERC20_ABI,
+        signer
+      );
+
+      const tx = await tokenContract.approve(contractAddress, amountToStake);
+      await tx.wait();
+
+      return true;
+    } catch (error) {
+      console.error("Error approving:", error);
+      return false;
+    }
+  }
+
+  const handleTransaction = async (transactionType) => {
+    setIsLoading(true);
+    try {
+      const signer = await provider.getSigner(0);
+      const amountInWei = ethers.parseUnits(amount.toString(), "wei");
+
+      if (signer) {
+        const contractWithSigner = initializeContract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        if (transactionType === "stake") {
+          const approved = await handleApprove(signer, amountInWei);
+          if (approved) {
+            const tx = await contractWithSigner.deposit(amountInWei);
+            await tx.wait();
+            startTimer();
+            alert("Staked successfully!");
+          }
+        } else if (transactionType === "unstake") {
+          const tx = await contractWithSigner.withdraw(amountInWei);
+          await tx.wait();
+          stopTimer();
+          alert("Unstaked successfully!");
+        }
+      }
+    } catch (error) {
+      console.error(`Error ${transactionType}ing:`, error);
+      alert(
+        `${
+          transactionType.charAt(0).toUpperCase() + transactionType.slice(1)
+        }ing failed!`
+      );
+    } finally {
+      setIsLoading(false); 
+    }
+  };
+
+  async function getBlockTime() {
+    try {
+      const blockNumber = await provider.getBlockNumber();
+
+      const block = await provider.getBlock(blockNumber);
+      const previousBlock = await provider.getBlock(blockNumber - 1);
+
+      const blockTime = block.timestamp - previousBlock.timestamp;
+
+      return blockTime;
+    } catch (error) {
+      console.error("Error fetching block time:", error);
+      return null;
+    }
+  }
+
+  const blocksPerYearCalc = (blockTimeSeconds) => {
+    const SECONDS_IN_YEAR = 365 * 24 * 60 * 60; 
+
+    return SECONDS_IN_YEAR / blockTimeSeconds;
+  };
+
+  async function calculateAPR() {
+    if (provider) {
+      const signer = await provider.getSigner(0);
+
+      if (signer) {
+        const contractWithSigner = initializeContract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const rewardPerBlock = await contractWithSigner.rewardPerBlock();
+        const totalStakedToken = await contractWithSigner.totalStakedToken();
+        const blockTimeSeconds = await getBlockTime();
+        const blocksPerYear = BigInt(blocksPerYearCalc(blockTimeSeconds));
+        let calculatedAPR = Number(
+          (blocksPerYear * rewardPerBlock) / totalStakedToken
+        );
+        setAPR(calculatedAPR);
+      }
+    }
+  }
+
+  const APRComponent = () => {
+    return (
+      <>
+        <div className="flex items-center space-x-4">
+          <h2 className="text-2xl mb-0 font-bold">APR: {protocolAPR}%</h2>
+          <button
+            onClick={calculateAPR}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Refresh APR
+          </button>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <div className="max-w-xl mx-auto bg-white p-4 rounded-md shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Staking App</h1>
+        {isLoading && (
+          <span className="inline-block ml-4">
+            <div className="loader w-6 h-6 border-t-2 border-b-2 border-blue-500 rounded-full animate-spin"></div>
+          </span>
+        )}
+        <div>
+          <APRComponent />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            Amount (Wei)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <button
+            className="bg-blue-500 text-white p-2 rounded-md w-full mb-2"
+            onClick={() => handleTransaction("stake")}
+          >
+            Stake
+          </button>
+
+          <button
+            className="bg-red-500 text-white p-2 rounded-md w-full"
+            onClick={() => handleTransaction("unstake")}
+          >
+            Unstake
+          </button>
+        </div>
+
+        <div>
+          <h2 className="text-xl mb-2">Staked For: {elapsedTime}</h2>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
