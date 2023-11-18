71426bfbf5f1bc65043a0c0ccbd8833b22bc6379
FiroshV <firosh.vasudevan@gmail.com>
2023-11-18 23:02:22 +0530

    first commit

--- README.md
+++ README.md
@@ -1,63 +1,70 @@
-# Staking App
 
-This is a simple staking application that allows users to stake their tokens and track their staked duration. The application is built using React and the ethers.js library to interact with the Ethereum blockchain. The application is set up to work with the Fantom testnet.
+# nUSDStablecoin Frontend (Next.js)
+
+## Introduction
+
+This is the frontend application for the `nUSDStablecoin` project, built using Next.js. It provides a user interface for interacting with the nUSDStablecoin smart contract on the Ethereum blockchain.
 
 ## Features
 
-- Display the APR (Annual Percentage Rate) for staking.
-- Allow users to stake and unstake tokens.
-- Display the elapsed time since the user staked their tokens.
-- Dynamically calculate and refresh the APR.
-  
+- User-friendly interface to interact with the nUSDStablecoin contract.
+- Integration with Ethereum blockchain using Web3 technologies.
+- Responsive design for various devices and screen sizes.
+
 ## Prerequisites
 
-- Node.js and npm installed.
-- MetaMask or a similar Ethereum wallet browser extension for interacting with the app.
+- Node.js and npm/yarn installed.
+- A modern web browser with JavaScript enabled.
+- Metamask or other Ethereum wallet browser extension for blockchain interactions.
 
-## Quick Start
+## Environment Setup
 
-1. **Clone the repository**:
+Create a `.env.local` file in the root directory and add the following:
 
-   ```bash
-   git clone https://github.com/FiroshV/fantom-stake-portal.git
-   ```
+```
+NEXT_PUBLIC_CONTRACT_ADDRESS=<Your Contract Address>
+NEXT_PUBLIC_SEPOLIA_RPC_URL=<Your Sepolia RPC URL>
+NEXT_PUBLIC_SEPOLIA_CHAIN_ID=<Your Sepolia Chain ID>
+```
 
-2. **Navigate to the project directory**:
+Replace the placeholders with the actual contract address and Sepolia network details.
 
-   ```bash
-   cd fantom-stake-portal
-   ```
+## Installation
 
-3. **Install the required dependencies**:
+1. Clone the frontend repository.
+2. Install dependencies:
 
    ```bash
    npm install
    ```
-
-4. **Run the development server**:
-
+   or
    ```bash
-   npm run dev
+   yarn install
    ```
 
-5. **Open a web browser** and navigate to `http://localhost:3000` to access the Staking App.
+## Running the Application
 
-## Minting Staking Tokens
+To run the application in development mode:
 
-Before staking tokens, you may need to mint or acquire the staking tokens used in this application. You can interact with the token contract and mint tokens using the following link:
+```bash
+npm run dev
+```
+or
+```bash
+yarn dev
+```
 
-[https://testnet.ftmscan.com/address/0x5b96e18cff249cf70f35f034e7606e850d5cda6c](https://testnet.ftmscan.com/address/0x5b96e18cff249cf70f35f034e7606e850d5cda6c)
+Visit `http://localhost:3000` in your browser to view the application.
 
-## Contract Details
+## Building for Production
 
-The staking contract used in this application is available for review and interaction on the Fantom testnet. You can access the contract and its code using the following link:
+To build the application for production, run:
 
-[https://testnet.ftmscan.com/address/0x8699d52c6c82b588bc12b46ada750dc0eb298818](https://testnet.ftmscan.com/address/0x8699d52c6c82b588bc12b46ada750dc0eb298818)
+```bash
+npm run build
+```
+or
+```bash
+yarn build
+```
 
-## Code Structure
-
-The main code resides in the `Home` component. The component sets up the connection with the Ethereum blockchain, allows users to stake and unstake their tokens, and calculates/display the APR.
-
-## Errors and Logging
-
-Make sure to monitor the browser console for any errors or logs during the staking/unstaking processes. The application provides appropriate error messages and logs for better debugging.
--- pages/index.js
+++ pages/index.js
@@ -1,202 +1,113 @@
-import { useState, useEffect, useRef } from "react";
-const { ethers } = require("ethers");
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import contractABI from "../ABI.json";
-import ERC20_ABI from "../ERC20_ABI.json";
 
-const contractAddress = "0x8699d52C6c82B588Bc12B46aDA750dC0EB298818";
-const TOKEN_ADDRESS = "0x5b96E18Cff249Cf70F35F034E7606E850D5CDa6C";
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+const SEPOLIA_CHAIN_ID = process.env.NEXT_PUBLIC_SEPOLIA_CHAIN_ID;
+const SEPOLIA_RPC_URL = process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL;
 
 export default function Home() {
+  const [totalSupply, setTotalSupply] = useState(0);
   const [amount, setAmount] = useState(0);
   const [provider, setProvider] = useState(null);
-
-  const [protocolAPR, setAPR] = useState(0);
-  const [elapsedTime, setElapsedTime] = useState("0s");
-
-  const timerIntervalRef = useRef(null);
-
-  const elapsedSecondsRef = useRef(0);
 
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (window && window.ethereum) {
       setProvider(new ethers.BrowserProvider(window.ethereum));
+    } else {
+      setProvider(new ethers.JsonRpcProvider(SEPOLIA_RPC_URL));
     }
   }, []);
 
   useEffect(() => {
-    calculateAPR();
+    calculateTotalSupply();
   }, [provider]);
-
-  const formatElapsedTime = (totalSeconds) => {
-    const seconds = totalSeconds % 60;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const days = Math.floor(totalSeconds / 86400);
-
-    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-  };
-
-  const startTimer = () => {
-    elapsedSecondsRef.current = 0;
-    setElapsedTime(formatElapsedTime(0));
-
-    timerIntervalRef.current = setInterval(() => {
-      elapsedSecondsRef.current += 1;
-      setElapsedTime(formatElapsedTime(elapsedSecondsRef.current));
-    }, 1000);
-  };
-
-  const stopTimer = () => {
-    if (timerIntervalRef.current) {
-      clearInterval(timerIntervalRef.current);
-      timerIntervalRef.current = null;
-      elapsedSecondsRef.current = 0;
-      setElapsedTime(formatElapsedTime(0));
-    }
-  };
 
   const initializeContract = (address, ABI, signer) => {
     return new ethers.Contract(address, ABI, signer || provider);
   };
 
-  async function handleApprove(signer, amountToStake) {
-    try {
-      const tokenContract = initializeContract(
-        TOKEN_ADDRESS,
-        ERC20_ABI,
-        signer
-      );
-
-      const tx = await tokenContract.approve(contractAddress, amountToStake);
-      await tx.wait();
-
-      return true;
-    } catch (error) {
-      console.error("Error approving:", error);
-      return false;
-    }
-  }
-
   const handleTransaction = async (transactionType) => {
     setIsLoading(true);
     try {
+      if (!amount > 0) {
+        return alert("Amount should be greater than zero");
+      }
+      const network = await provider.getNetwork();
+      if (network.chainId.toString() !== SEPOLIA_CHAIN_ID) {
+        return alert("Please connect to the Sepolia network.");
+      }
       const signer = await provider.getSigner(0);
+      if (!signer) {
+        return alert("Signer not found. Please connect to MetaMask.");
+      }
+
       const amountInWei = ethers.parseUnits(amount.toString(), "wei");
 
       if (signer) {
         const contractWithSigner = initializeContract(
-          contractAddress,
+          CONTRACT_ADDRESS,
           contractABI,
           signer
         );
 
-        if (transactionType === "stake") {
-          const approved = await handleApprove(signer, amountInWei);
-          if (approved) {
-            const tx = await contractWithSigner.deposit(amountInWei);
-            await tx.wait();
-            startTimer();
-            alert("Staked successfully!");
-          }
-        } else if (transactionType === "unstake") {
-          const tx = await contractWithSigner.withdraw(amountInWei);
+        if (transactionType === "deposit") {
+          const tx = await contractWithSigner.depositETH({
+            value: amountInWei,
+          });
           await tx.wait();
-          stopTimer();
-          alert("Unstaked successfully!");
+        } else if (transactionType === "redeem") {
+          const tx = await contractWithSigner.redeemETH(amountInWei);
+          await tx.wait();
         }
       }
+
+      await calculateTotalSupply();
     } catch (error) {
-      console.error(`Error ${transactionType}ing:`, error);
-      alert(
-        `${
-          transactionType.charAt(0).toUpperCase() + transactionType.slice(1)
-        }ing failed!`
-      );
+      return alert("An error occurred while connecting to MetaMask. Please make sure MetaMask is installed and you are logged in.");   
     } finally {
       setIsLoading(false);
     }
   };
 
-  async function getBlockTime() {
-    try {
-      const blockNumber = await provider.getBlockNumber();
-
-      const block = await provider.getBlock(blockNumber);
-      const previousBlock = await provider.getBlock(blockNumber - 1);
-
-      const blockTime = block.timestamp - previousBlock.timestamp;
-
-      return blockTime;
-    } catch (error) {
-      console.error("Error fetching block time:", error);
-      return null;
-    }
-  }
-
-  const blocksPerYearCalc = (blockTimeSeconds) => {
-    const SECONDS_IN_YEAR = 365 * 24 * 60 * 60;
-
-    return SECONDS_IN_YEAR / blockTimeSeconds;
-  };
-
-  async function calculateAPR() {
+  async function calculateTotalSupply() {
     if (provider) {
-      const signer = await provider.getSigner(0);
+      const contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        contractABI,
+        provider
+      );
 
-      if (signer) {
-        const contractWithSigner = initializeContract(
-          contractAddress,
-          contractABI,
-          signer
+      try {
+        const _totalSupply = await contract.getTotalnUSDSupply();
+        setTotalSupply(
+          _totalSupply ? Number(_totalSupply / (BigInt(10) ** BigInt(18))) : 0
         );
-
-        const rewardPerBlock = await contractWithSigner.rewardPerBlock();
-        const totalStakedToken = await contractWithSigner.totalStakedToken();
-        const blockTimeSeconds = await getBlockTime();
-        const blocksPerYear = BigInt(blocksPerYearCalc(blockTimeSeconds));
-        let calculatedAPR =
-          Number((blocksPerYear * rewardPerBlock * 100n) / totalStakedToken) /
-          100;
-        setAPR(calculatedAPR);
+      } catch (error) {
+        console.error("Error fetching total supply:", error);
+        setTotalSupply(0);
       }
     }
   }
 
-  const APRComponent = () => {
-    return (
-      <>
-        <div className="flex items-center space-x-4">
-          <h2 className="text-2xl mb-0 font-bold">APR: {protocolAPR}%</h2>
-          <button
-            onClick={calculateAPR}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
-          >
-            Refresh APR
-          </button>
-        </div>
-      </>
-    );
-  };
-
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <div className="max-w-xl mx-auto bg-white p-4 rounded-md shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Staking App</h1>
+        <h1 className="text-center text-2xl font-bold mb-4">
+          nUSD Stable coin
+        </h1>
         {isLoading && (
           <span className="inline-block ml-4">
             <div className="loader w-6 h-6 border-t-2 border-b-2 border-blue-500 rounded-full animate-spin"></div>
           </span>
         )}
         <div>
-          <APRComponent />
+          <h2 className="text-lg mb-0">Total Supply : {totalSupply} nUSD</h2>
         </div>
 
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Amount (Wei)
-          </label>
           <input
             type="number"
             value={amount}
@@ -205,24 +116,20 @@ export default function Home() {
           />
         </div>
 
-        <div className="mb-4">
+        <div className="mb-4 flex gap-4">
           <button
-            className="bg-blue-500 text-white p-2 rounded-md w-full mb-2"
-            onClick={() => handleTransaction("stake")}
+            className="bg-blue-500 text-white p-2 rounded-md w-full"
+            onClick={() => handleTransaction("deposit")}
           >
-            Stake
+            Deposit
           </button>
 
           <button
-            className="bg-red-500 text-white p-2 rounded-md w-full"
-            onClick={() => handleTransaction("unstake")}
+            className="bg-purple-500 text-white p-2 rounded-md w-full"
+            onClick={() => handleTransaction("redeem")}
           >
-            Unstake
+            Redeem
           </button>
-        </div>
-
-        <div>
-          <h2 className="text-xl mb-2">Staked For: {elapsedTime}</h2>
         </div>
       </div>
     </div>
